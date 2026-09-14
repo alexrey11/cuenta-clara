598dc4d230c45d7a25182fdfc2cec4ea6687b8dc
alexrey11 <alejandrocuestarey@gmail.com>
2026-09-14 09:41:48 -0400

    arreglado

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- Importamos el plugin de Tailwind v4
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/cuenta-clara/', // <--- ¡ESTA LÍNEA ES LA CLAVE!
   plugins: [
     react(),
-    tailwindcss(), // <-- Lo activamos aquí
+    tailwindcss(),
   ],
 })
